edcbdd974793278e4aafd4ed1b2bb56c2220eb1a
hugo1456 <hugo1456@gmail.com>
2014-06-18 18:40:52 +0900

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.1.1'
 gem 'faker', '1.1.2'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
 
 group :development, :test do
   gem 'sqlite3'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,48 +1,27 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
     
-    it "should have the content 'Welcome'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Welcome')
-    end
-
-    it "should have the title 'Home'" do
-      visit'/static_pages/home'
-      expect(page).to have_title('Project')
-    end
-    
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('Home')
-    end
+    it { should have_content('Welcome') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('Home') }
   end
 
   describe "Help page" do
-    
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title('Help')
-    end
+    before { visit help_path } 
+  
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before {visit about_path }
     
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title('About Us')
-    end
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 end
