a2a5a95de20c7018c18fe599b19438a57dea7170
C9hugo1456 <hugo1456@gmail.com>
2014-09-23 06:35:20 +0000

    SQLite3

--- db/schema.rb
+++ db/schema.rb
@@ -80,15 +80,6 @@ ActiveRecord::Schema.define(version: 20140828061400) do
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
-  create_table "statuses", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "statuses", ["user_id", "created_at"], name: "index_statuses_on_user_id_and_created_at"
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
