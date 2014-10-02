d668ffcb70634a233d94ba947d0d0030f82db895
C9hugo1456 <hugo1456@gmail.com>
2014-10-02 05:29:25 +0000

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,5 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :first_name, :last_name, :bio, :slug) }
     end
-  end
+end
 
--- lib/mercury/authentication.rb
+++ lib/mercury/authentication.rb
@@ -1,12 +1,8 @@
 module Mercury
   module Authentication
-
+    
     def can_edit?
-      if :authenticate_user! && current_user?(@user)
-        true # check here to see if the user is logged in/has access
-      else
-        redirect_to root_path
-      end
+      false # check here to see if the user is logged in/has access
     end
 
   end
