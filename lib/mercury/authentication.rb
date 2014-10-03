6a400c28147e200a13ec6b9b0b5593f6b29d37bb
C9hugo1456 <hugo1456@gmail.com>
2014-10-03 12:52:41 +0000

    Minor changes

--- lib/mercury/authentication.rb
+++ lib/mercury/authentication.rb
@@ -2,7 +2,7 @@ module Mercury
   module Authentication
     
     def can_edit?
-      false # check here to see if the user is logged in/has access
+      true # check here to see if the user is logged in/has access
     end
 
   end
